2e2bb915165646cdfbfc3f7707cb3b5a9ebd7bfe
sergiuplesco <plescosergiu@gmail.com>
2021-06-15 20:20:03 +0300

    Final deployment prep

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./config.env" });
 
 import express from "express";
+import path from "path";
 import NoteRoutes from "./routes/note.routes.js";
 import AuthRoutes from "./routes/auth.routes.js";
 
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 3001;
 connectDB();
 
 app.use(cors());
+
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -26,6 +28,18 @@ app.use((req, res) => {
 
 // Error Handler (Should be lat piece of middleware)
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+	});
+} else {
+	app.get("/", (req, res) => {
+		res.send("api running");
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`App listen on PORT: ${PORT}`);
