167c4094d8fbb33ccf5e95f0283c4e6bb24f0e8d
sergiuplesco <plescosergiu@gmail.com>
2021-06-16 19:18:09 +0300

    /

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use((req, res) => {
 // Error Handler (Should be lat piece of middleware)
 app.use(errorHandler);
 
-app.use(express.static(path.join(__dirname, "../", "frontend", "build")));
+app.use("/", express.static(path.join(__dirname, "../frontend/build")));
 console.log("static path", path.join(__dirname, "../", "frontend", "build"));
 
 app.get("*", (req, res) => {
