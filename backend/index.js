b9219f9eec374f09d29b3147c65c000edda08708
sergiuplesco <plescosergiu@gmail.com>
2021-06-16 10:38:46 +0300

    removed __dirname and use path.dirname()

--- backend/index.js
+++ backend/index.js
@@ -33,10 +33,10 @@ app.use((req, res) => {
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/build")));
+	app.use(express.static(path.dirname("/frontend/build")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+		res.sendFile(path.dirname("frontend", "build", "index.html"));
 	});
 } else {
 	app.get("/", (req, res) => {
