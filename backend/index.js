e2e079521baa215d09c8172d988b9c2f05d5e6e5
sergiuplesco <plescosergiu@gmail.com>
2021-06-16 09:55:01 +0300

    changed import to require of path pkg

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,8 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./config.env" });
 
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const path = require("path");
+
 import NoteRoutes from "./routes/note.routes.js";
 import AuthRoutes from "./routes/auth.routes.js";
 
@@ -33,10 +31,10 @@ app.use((req, res) => {
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.resolve(__dirname, "/frontend/build")));
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+		res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 	});
 } else {
 	app.get("/", (req, res) => {
