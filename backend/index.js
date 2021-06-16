dbd0936d25722c53cf2253edc376bcc49bd3c196
sergiuplesco <plescosergiu@gmail.com>
2021-06-16 20:38:20 +0300

    mmm

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(AuthRoutes);
 app.use(NoteRoutes);
 
 app.use((req, res) => {
-	res.status(404).send("page not found");
+	res.status(404).send("ppppage not found");
 });
 
 // Error Handler (Should be lat piece of middleware)
